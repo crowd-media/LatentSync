825367164581802f21a07bc8f7b5e99736a11a8e
itziar <itziar@unith.ai>
2025-09-10 14:46:48 +0000

    always positive inputs, no random dataset

--- latentsync/data/syncnet_dataset.py
+++ latentsync/data/syncnet_dataset.py
@@ -60,10 +60,10 @@ class SyncNetDataset(Dataset):
         end_idx = start_idx + self.mel_window_length
         return original_mel[:, start_idx:end_idx].unsqueeze(0)
 
-    def get_frames(self, video_reader: VideoReader):
+    def get_frames(self, video_reader: VideoReader, start_idx):
         total_num_frames = len(video_reader)
 
-        start_idx = random.randint(0, total_num_frames - self.num_frames)
+        # start_idx = random.randint(0, total_num_frames - self.num_frames)
         frames_index = np.arange(start_idx, start_idx + self.num_frames, dtype=int)
 
         while True:
@@ -76,63 +76,60 @@ class SyncNetDataset(Dataset):
         frames = video_reader.get_batch(frames_index).asnumpy()
         wrong_frames = video_reader.get_batch(wrong_frames_index).asnumpy()
 
-        return frames, wrong_frames, start_idx
+        return frames, wrong_frames
 
     def worker_init_fn(self, worker_id):
         self.worker_id = worker_id
 
     def __getitem__(self, idx):
-        while True:
+        idx = random.randint(0, len(self) - 1)
+
+        # Get video file path
+        video_path = self.video_paths[idx]
+
+        vr = VideoReader(video_path, ctx=cpu(self.worker_id))      
+
+        if len(vr) < 2 * self.num_frames:
+            exit(0)
+
+        for start_idx in range(len(vr)- self.num_frames):
+
+            frames, wrong_frames= self.get_frames(vr, start_idx)
+
+        mel_cache_path = os.path.join(
+            self.audio_mel_cache_dir, os.path.basename(video_path).replace(".mp4", "_mel.pt")
+        )
+
+        if os.path.isfile(mel_cache_path):
             try:
-                idx = random.randint(0, len(self) - 1)
+                original_mel = torch.load(mel_cache_path, weights_only=True)
+            except Exception as e:
+                print(f"{type(e).__name__} - {e} - {mel_cache_path}")
+                os.remove(mel_cache_path)
+                original_mel = self.read_audio(video_path)
+                torch.save(original_mel, mel_cache_path)
+        else:
+            original_mel = self.read_audio(video_path)
+            torch.save(original_mel, mel_cache_path)
 
-                # Get video file path
-                video_path = self.video_paths[idx]
+        mel = self.crop_audio_window(original_mel, start_idx)
 
-                vr = VideoReader(video_path, ctx=cpu(self.worker_id))
+        if mel.shape[-1] != self.mel_window_length:
+            exit(0)
 
-                if len(vr) < 2 * self.num_frames:
-                    continue
+        y = torch.ones(1).float()
+        chosen_frames = frames
 
-                frames, wrong_frames, start_idx = self.get_frames(vr)
+        # if random.choice([True, False]):
+        #     y = torch.ones(1).float()
+        #     chosen_frames = frames
+        # else:
+        #     y = torch.zeros(1).float()
+        #     chosen_frames = wrong_frames
 
-                mel_cache_path = os.path.join(
-                    self.audio_mel_cache_dir, os.path.basename(video_path).replace(".mp4", "_mel.pt")
-                )
+        chosen_frames = self.image_processor.process_images(chosen_frames)
 
-                if os.path.isfile(mel_cache_path):
-                    try:
-                        original_mel = torch.load(mel_cache_path, weights_only=True)
-                    except Exception as e:
-                        print(f"{type(e).__name__} - {e} - {mel_cache_path}")
-                        os.remove(mel_cache_path)
-                        original_mel = self.read_audio(video_path)
-                        torch.save(original_mel, mel_cache_path)
-                else:
-                    original_mel = self.read_audio(video_path)
-                    torch.save(original_mel, mel_cache_path)
-
-                mel = self.crop_audio_window(original_mel, start_idx)
-
-                if mel.shape[-1] != self.mel_window_length:
-                    continue
-
-                if random.choice([True, False]):
-                    y = torch.ones(1).float()
-                    chosen_frames = frames
-                else:
-                    y = torch.zeros(1).float()
-                    chosen_frames = wrong_frames
-
-                chosen_frames = self.image_processor.process_images(chosen_frames)
-
-                vr.seek(0)  # avoid memory leak
-                break
-
-            except Exception as e:  # Handle the exception of face not detcted
-                print(f"{type(e).__name__} - {e} - {video_path}")
-                if "vr" in locals():
-                    vr.seek(0)  # avoid memory leak
+        vr.seek(0)  # avoid memory leak
 
         sample = dict(frames=chosen_frames, audio_samples=mel, y=y)
 
